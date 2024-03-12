38c4b868d44bb54b4ebd954c9d2ba0a6146fa127
CoderRohan003 <rohandutta756@gmail.com>
2024-03-13 00:43:55 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const path = require("path")
+const PORT = 8000;
+
+mongoose.connect('mongodb://localhost:27017/blogger').then((e) => console.log("MongoDB Connected"));
+
+const UserRoute = require("./routes/user")
+
+app.set('view engine' , 'ejs');
+app.set('views', path.resolve("./views"));
+
+app.use(express.urlencoded({extended:false}));
+
+app.get("/", (req,res) => {
+    res.render("home");
+});
+
+app.use("/user" , UserRoute);
+
+
+app.listen(PORT , () => console.log(`Server started at PORT : ${PORT}`));
+
---
+++ models/user.js
@@ -0,0 +1,74 @@
+const { Schema, model } = require("mongoose");
+const { randomBytes, createHmac } = require('crypto');  /// Built in package
+const { createTokenForUser } = require("../services/authentication");
+
+const userSchema = new Schema({
+    fullName:{
+        type:String,
+        required: true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique: true,
+    },
+    salt:{
+        type:String,
+        
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    profileImageURL:{
+        type:String,
+        default: '/images/default.png',
+    },
+    role: {
+        type:String,
+        enum:["USER","ADMIN"],
+        default:"USER",
+    },
+},{ timestamps:true });
+
+userSchema.pre("save", function (next) {
+    const user = this;     // pointing to present user
+
+    if (!user.isModified("password")) {
+        return; 
+    }
+
+    // Crypting the password of user        
+    const salt = randomBytes(16).toString();   // salt is a secret key for user password
+    const hashedPassword = createHmac("sha256",salt).update(user.password).digest("hex");
+    // sha256 is a encryption alogrithm
+
+    this.salt = salt;
+    this.password = hashedPassword;
+
+    next();
+
+});
+
+userSchema.static('matchPasswordAndGenerateToken' , async function(email, password){
+    const user = await this.findOne({email});
+    if(!user) throw new Error('User Not Found!!');
+
+    const salt = user.salt;
+    const hashedPassword = user.password;
+
+    const userProvidedHash = createHmac("sha256",salt).update(password).digest("hex");
+
+    if(hashedPassword !== userProvidedHash)
+        throw new Error('Incorrect Password !!');
+
+    const token = createTokenForUser(user);
+    return token;
+
+  
+    
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
