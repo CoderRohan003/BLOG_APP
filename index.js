ce0adbf3f1ce9260d6146611c3c1ac977b7e3bfa
CoderRohan003 <rohandutta756@gmail.com>
2024-03-13 02:51:48 +0530

    ghyughhu

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
 const path = require("path")
 const PORT = 8000;
 
 mongoose.connect('mongodb://localhost:27017/blogger').then((e) => console.log("MongoDB Connected"));
 
-const UserRoute = require("./routes/user")
+const UserRoute = require("./routes/user");
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 app.set('view engine' , 'ejs');
 app.set('views', path.resolve("./views"));
 
 app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
 
 app.get("/", (req,res) => {
-    res.render("home");
+    res.render("home",{
+        user: req.user,   // Passing user obj to home page 
+    });
 });
 
 app.use("/user" , UserRoute);
