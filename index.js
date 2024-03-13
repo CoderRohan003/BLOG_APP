2c1381a165d544c5cd73097d154edc3e51b9dd6f
CoderRohan003 <rohandutta756@gmail.com>
2024-03-13 10:46:42 +0530

    fdsgfegfdsf

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const PORT = 8000;
 mongoose.connect('mongodb://localhost:27017/blogger').then((e) => console.log("MongoDB Connected"));
 
 const UserRoute = require("./routes/user");
+const BlogRoute = require("./routes/blog");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 app.set('view engine' , 'ejs');
@@ -25,6 +26,7 @@ app.get("/", (req,res) => {
 });
 
 app.use("/user" , UserRoute);
+app.use("/blog" , BlogRoute);
 
 
 app.listen(PORT , () => console.log(`Server started at PORT : ${PORT}`));
